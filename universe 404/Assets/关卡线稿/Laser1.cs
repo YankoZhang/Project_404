7670cec14b0460875a177f8ec1bef2d5f1f1f3d9
Morty <2734228738@qq.com>
2022-04-04 00:41:37 +0800

    关卡

--- universe 404/Assets/关卡线稿/Laser1.cs
+++ universe 404/Assets/关卡线稿/Laser1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Laser : MonoBehaviour
+public class Laser1 : MonoBehaviour
 {
     public LineRenderer lineRenderer;
     // Start is called before the first frame update
@@ -16,7 +16,7 @@ public class Laser : MonoBehaviour
     {
         //…Ë÷√LineRendererdµƒŒª÷√
         lineRenderer.SetPosition(0, transform.position);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right);
         if (hit)
         {
             lineRenderer.SetPosition(1, hit.point);
